0833a1603010095e33cebb00a0e09c8c6456bf53
Andrew Chan <a258chan@uwaterloo.ca>
2021-06-09 18:16:55 -0400

    test queries

--- api/controllers/indexController.js
+++ api/controllers/indexController.js
@@ -13,5 +13,15 @@ class IndexController {
     });
   }
 
+  static getAnzeTestQuery(req, res, next) {
+    const db = req.db;
+
+    db.query('SELECT * FROM test WHERE text = \"this is a string from the db\"', (err, results, fields) => {
+      if (err) throw err;
+      console.log(results);
+      return res.send('Test output: ' + results[0]);
+    });
+  }
+
 }
 module.exports = IndexController;
