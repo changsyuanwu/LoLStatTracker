11f8f9cc7bae1aa4eb939b2b1a08c8b4847f64c8
The Master Dodo <william.wu1219@gmail.com>
2021-06-10 06:16:12 +0800

    test change

--- api/controllers/indexController.js
+++ api/controllers/indexController.js
@@ -12,6 +12,7 @@ class IndexController {
     // fields will contain information about the returned results fields (if any)
     db.query('SELECT * FROM test', (err, results, fields) => {
       if (err) throw err;
+      //testset
       return res.json(results);
     });
   }
