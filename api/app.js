377e3062ee8a68b6e2b25788f20a0f664c9aafbe
The Master Dodo <william.wu1219@gmail.com>
2021-06-10 04:49:55 +0800

    change var to const

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mysql = require('mysql');
 
 // Setup DB
@@ -22,10 +22,10 @@ db.connect(function (err) {
   console.log('connected to MySQL database');
 });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
