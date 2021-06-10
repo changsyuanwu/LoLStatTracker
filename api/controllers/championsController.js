1e87375b827653faaa207bdc32b156648cc41ff7
The Master Dodo <william.wu1219@gmail.com>
2021-06-10 22:51:14 +0800

    add champion winrate query endpoint

--- api/controllers/championsController.js
+++ api/controllers/championsController.js
@@ -1,18 +1,88 @@
 class ChampionsController {
 
-  static getChampionByName(req, res, next) {
+  static async getChampionByName(req, res, next) {
     // Get a champion by their name (case insensitive)
 
     const db = req.db;
 
-    const name = req.params.name;
+    const name = req.params.name.toLowerCase();
 
-    db.query('SELECT * FROM champions WHERE LOWER(champion_name) = ?', [name], (err, results, fields) => {
-      if (err) throw err;
-      return res.json(results);
-    });
+    try {
+      const [champStats] = await db.query('SELECT * FROM champions WHERE LOWER(champion_name) = ?', [name])
+      return res.json(champStats[0]);
+    }
+    catch (err) {
+      throw err;
+    }
   }
 
+  static async getChampionWinRateByName(req, res, next) {
+    const db = req.db;
+
+    const name = req.params.name.toLowerCase();
+
+    try {
+      const [numMatchesWon] = await db.query(
+        `SELECT COUNT(*) AS count FROM matches 
+        WHERE (
+          ? IN (
+            LOWER(blue_top), 
+            LOWER(blue_jungle), 
+            LOWER(blue_mid), 
+            LOWER(blue_adc), 
+            LOWER(blue_support)
+          )
+          AND result = 'Blue'
+        )
+        OR (
+          ? IN (
+            LOWER(red_top), 
+            LOWER(red_jungle), 
+            LOWER(red_mid), 
+            LOWER(red_adc), 
+            LOWER(red_support)
+          )
+          AND result = 'Red'
+        )`,
+        [name, name]
+      );
+
+      const [numMatchesPlayed] = await db.query(
+        `SELECT COUNT(*) AS count FROM matches 
+        WHERE (
+          ? IN (
+            LOWER(blue_top), 
+            LOWER(blue_jungle), 
+            LOWER(blue_mid), 
+            LOWER(blue_adc), 
+            LOWER(blue_support)
+          )
+        )
+        OR (
+          ? IN (
+            LOWER(red_top), 
+            LOWER(red_jungle), 
+            LOWER(red_mid), 
+            LOWER(red_adc), 
+            LOWER(red_support)
+          )
+        )`,
+        [name, name]
+      );
+
+      const winrate = numMatchesWon[0].count / numMatchesPlayed[0].count;
+
+      return res.json({ 
+        champion: name,
+        winrate: winrate,
+        won: numMatchesWon[0].count,
+        played: numMatchesPlayed[0].count
+      });
+    }
+    catch (err) {
+      throw err;
+    }
+  }
 }
 
 module.exports = ChampionsController;
--- api/routes/championsRouter.js
+++ api/routes/championsRouter.js
@@ -6,4 +6,7 @@ const ChampionsController = require('../controllers/championsController.js');
 /* GET champion by name */
 router.get('/:name', ChampionsController.getChampionByName);
 
+/* GET champion winrate by name */
+router.get('/winrate/:name', ChampionsController.getChampionWinRateByName);
+
 module.exports = router;
