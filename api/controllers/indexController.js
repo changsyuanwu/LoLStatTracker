b246021b7ed0520cd1a307dc5326f0ccc00b0874
The Master Dodo <william.wu1219@gmail.com>
2021-06-13 03:33:09 +0800

    remove test queries

--- api/controllers/indexController.js
+++ api/controllers/indexController.js
@@ -1,31 +1,6 @@
 class IndexController {
-
   static getIndex(req, res, next) {
     return res.send("I'm working");
   }
-
-  static getTestQuery(req, res, next) {
-    const db = req.db;
-
-    // err will be an Error if one occurred during the query
-    // results will be an array of the queried rows
-    // fields will contain information about the returned results fields (if any)
-    db.query('SELECT * FROM test', (err, results, fields) => {
-      if (err) throw err;
-      
-      return res.json(results);
-    });
-  }
-
-  static getAnzeTestQuery(req, res, next) {
-    const db = req.db;
-
-    db.query('SELECT * FROM test WHERE text = \"this is a string from the db\"', (err, results, fields) => {
-      if (err) throw err;
-      console.log(results);
-      return res.json(results);
-    });
-  }
-
 }
 module.exports = IndexController;
--- api/routes/indexRouter.js
+++ api/routes/indexRouter.js
@@ -6,8 +6,4 @@ const IndexController = require('../controllers/indexController.js');
 /* GET index */
 router.get('/', IndexController.getIndex);
 
-router.get('/test-query', IndexController.getTestQuery);
-
-router.get('/anze-test-query', IndexController.getAnzeTestQuery);
-
 module.exports = router;
