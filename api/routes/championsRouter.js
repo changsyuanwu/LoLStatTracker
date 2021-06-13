52b16ef64ff0594e61d3bd584d0c10ff5e60a87c
The Master Dodo <william.wu1219@gmail.com>
2021-06-13 11:13:00 +0800

    add description on how to use update-stats endpoint

--- api/routes/championsRouter.js
+++ api/routes/championsRouter.js
@@ -12,6 +12,13 @@ router.get('/play-stats/:name', ChampionsController.getChampionPlayStatsByName);
 /* GET a list of champion names in an array */
 router.get('/list', ChampionsController.getChampionNamesList);
 
+/* POST to change the base stats of a champion 
+ *   name: name of the champion that is being edited
+ *   stat: name of the stat being edited
+ *   value: value to set the stat to
+ *
+ * Example: .../champions/update-stats?name=ezreal&stat=mana&value=1
+ */
 router.post('/update-stats', ChampionsController.updateChampionBaseStats);
 
 module.exports = router;
