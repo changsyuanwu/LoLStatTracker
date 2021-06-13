011cc4be50dda21a6aa757b74e4fc06a34d32353
The Master Dodo <william.wu1219@gmail.com>
2021-06-13 14:31:48 +0800

    added cross-origin-resource-sharing

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mysql = require('mysql2');
+const cors = require('cors');
 
 // Setup DB
 let db = mysql.createPool({
@@ -39,11 +40,13 @@ app.use('/matches', matchesRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Module setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
