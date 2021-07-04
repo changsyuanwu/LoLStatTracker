e93f782e0e1de21ff2c1ab11c0ab7595937d5dd5
The Master Dodo <william.wu1219@gmail.com>
2021-07-05 06:56:02 +0800

    log response on test champ stat update

--- api/tests/testUpdateChampionStat.js
+++ api/tests/testUpdateChampionStat.js
@@ -4,4 +4,6 @@ const fetch = require('node-fetch');
 fetch("http://localhost:9000/champions/update-stats?name=ezreal&stat=mana&value=1", {
   method: 'PUT',
   headers: { 'Content-Type': 'application/json' }
-});
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
