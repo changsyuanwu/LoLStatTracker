9cdc5ff9309fc6dec3a5539ef195aa0f32c138ee
The Master Dodo <william.wu1219@gmail.com>
2021-07-05 06:49:02 +0800

    update test.js

--- api/tests/test.js
+++ api/tests/test.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-
+// set Ezreal's mana stat to 1
 fetch("http://localhost:9000/champions/update-stats?name=ezreal&stat=mana&value=1", {
   method: 'PUT',
   headers: { 'Content-Type': 'application/json' }
