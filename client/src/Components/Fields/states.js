3184c541c60cd0d21b8f45fa709ad85e55009fcc
jenishpatel2147 <47545545+jenishpatel2147@users.noreply.github.com>
2021-07-04 22:42:25 -0400

    Implementing Dashboard for champion players

--- client/src/Components/Fields/states.js
+++ client/src/Components/Fields/states.js
@@ -5,8 +5,11 @@ const initial = {
 const roots = (state = initial, action) => {
     switch (action.type) {
         case "ALL":
-            console.log(action);
+            //console.log(action);
             return Object.assign({ data: action.data });
+        case "MERGED":
+            let joined = state.data.concat(action.data);
+            return Object.assign({ data: joined });
         default:
             return state;
     }
